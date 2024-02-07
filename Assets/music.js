8b83e2b10e7e5b4ef7e92884da23cc74f2325c93
Kumar Kartikay <140949448+KKartikay-27@users.noreply.github.com>
2024-02-07 15:58:04 +0530

    Update music.js

--- Assets/music.js
+++ Assets/music.js
@@ -1,62 +1,62 @@
 let songs = [
     {
         name: 'Kinni Kinni',
-        path: '/Assets/Music/Kinni Kinni.mp3',
+        path: 'Assets/Music/Kinni Kinni.mp3',
         artist: 'Diljit Dosanjh',
-        cover: '/Assets/Cover/Kinni Kinni.jpg'
+        cover: 'Assets/Cover/Kinni Kinni.jpg'
     },
     {
         name: 'Softly',
-        path: '/Assets/Music/Softly.mp3',
+        path: 'Assets/Music/Softly.mp3',
         artist: 'Karan Aujla',
-        cover: '/Assets/Cover/Softly.jpg'
+        cover: 'Assets/Cover/Softly.jpg'
     },
     {
         name: 'One Love',
-        path: '/Assets/Music/One Love.mp3',
+        path: 'Assets/Music/One Love.mp3',
         artist: 'Shubh',
-        cover: '/Assets/Cover/One Love.jpg'
+        cover: 'Assets/Cover/One Love.jpg'
     },
     {
         name: 'Sunflower',
-        path: '/Assets/Music/Sunflower.mp3',
+        path: 'Assets/Music/Sunflower.mp3',
         artist: 'Post Malone & Swae Lee',
-        cover: '/Assets/Cover/Sunflower.jpg'
+        cover: 'Assets/Cover/Sunflower.jpg'
     },
     {
         name: 'Until I Found You',
-        path: '/Assets/Music/Until I Found You.mp3',
+        path: 'Assets/Music/Until I Found You.mp3',
         artist: 'Stephen Sanchez & Em Beihold',
-        cover: '/Assets/Cover/Until I Found You.jpeg'
+        cover: 'Assets/Cover/Until I Found You.jpeg'
     },
     {
         name: 'Udd Gaye',
-        path: '/Assets/Music/Udd Gaye.mp3',
+        path: 'Assets/Music/Udd Gaye.mp3',
         artist: 'Ritviz',
-        cover: '/Assets/Cover/Udd Gaye.jpg'
+        cover: 'Assets/Cover/Udd Gaye.jpg'
     },
     {
         name: 'Hass Hass',
-        path: '/Assets/Music/Hass Hass.mp3',
+        path: 'Assets/Music/Hass Hass.mp3',
         artist: 'Diljit Dosanjh',
-        cover: '/Assets/Cover/Hass Hass.jpg'
+        cover: 'Assets/Cover/Hass Hass.jpg'
     },
     {
         name: 'Attention',
-        path: '/Assets/Music/Attention.mp3',
+        path: 'Assets/Music/Attention.mp3',
         artist: 'Charlie Puth',
-        cover: '/Assets/Cover/Attention.jpg'
+        cover: 'Assets/Cover/Attention.jpg'
     },
     {
         name: 'One Dance',
-        path: '/Assets/Music/One Dance.mp3',
+        path: 'Assets/Music/One Dance.mp3',
         artist: 'Drake ft. Wizkid & Kyla',
-        cover: '/Assets/Cover/One Dance.jpeg'
+        cover: 'Assets/Cover/One Dance.jpeg'
     },
     {
         name: 'Heat Waves',
-        path: '/Assets/Music/Heat Waves.mp3',
+        path: 'Assets/Music/Heat Waves.mp3',
         artist: 'Glass Animals',
-        cover: '/Assets/Cover/Heat Waves.png'
+        cover: 'Assets/Cover/Heat Waves.png'
     }
 ]
